3ff0d43f786564bfed0c8843dbb0145e63563668
Alexander Christensen <alexpaulchristensen@gmail.com>
2020-12-04 12:54:12 -0500

    added preprocessing data

---
+++ R/open.preprocess.R
@@ -0,0 +1,26 @@
+# Preprocessed \code{\link[SemNetCleaner]{textcleaner}} Object (Openness and Verbal Fluency)----
+#' Preprocessed \code{\link[SemNetCleaner]{textcleaner}} Object (Openness and Verbal Fluency)
+#'
+#' Preprocessed \code{\link[SemNetCleaner]{textcleaner}} object for the Animals verbal fluency data (\emph{n} = 516)
+#' from Christensen and Kenett (2020).
+#' 
+#' @name open.clean
+#' 
+#' @docType data
+#' 
+#' @usage data(open.preprocess)
+#' 
+#' @format open.preprocess (list, length = 4)
+#' 
+#' @keywords datasets
+#' 
+#' @references
+#' Christensen, A. P., & Kenett, Y. N. (2020).
+#' Semantic network analysis (SemNA): A tutorial on preprocessing, estimating, and analyzing semantic networks.
+#' \emph{PsyArxiv}.
+#' doi:\href{https://doi.org/10.31234/osf.io/eht87}{10.31234/osf.io/eht87}
+#' 
+#' @examples 
+#' data("open.preprocess")
+NULL
+#----
