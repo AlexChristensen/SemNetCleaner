0d003d2b2904cd80ca6e96de70e6720ab873a759
Alexander Christensen <alexpaulchristensen@gmail.com>
2021-08-16 11:53:46 -0400

    removed DOIs for CRAN checks

--- R/open.preprocess.R
+++ R/open.preprocess.R
@@ -18,7 +18,6 @@
 #' Christensen, A. P., & Kenett, Y. N. (2020).
 #' Semantic network analysis (SemNA): A tutorial on preprocessing, estimating, and analyzing semantic networks.
 #' \emph{PsyArxiv}.
-#' doi:\href{https://doi.org/10.31234/osf.io/eht87}{10.31234/osf.io/eht87}
 #' 
 #' @examples 
 #' data("open.preprocess")
