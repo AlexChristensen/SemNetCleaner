67ba90eaca61fe8b8b7a8b119485888a4791680d
Alexander Christensen <alexpaulchristensen@gmail.com>
2021-12-18 08:53:18 -0500

    Update utils-SemNetCleaner.R

--- R/utils-SemNetCleaner.R
+++ R/utils-SemNetCleaner.R
@@ -926,17 +926,17 @@ textcleaner.fluency <- function(
 # Major update: 19.04.2020
 # Added type of task: 21.10.2021
 
-data = response_matrix
-type = "free"
-dictionary = "coca"
-spelling = "US"
-miss = 99
-add.path = NULL
-keepStrings = FALSE
-allowPunctuations = "-"
-allowNumbers = FALSE
-lowercase = TRUE
-continue = NULL
+# data = response_matrix
+# type = "free"
+# dictionary = "coca"
+# spelling = "US"
+# miss = 99
+# add.path = NULL
+# keepStrings = FALSE
+# allowPunctuations = "-"
+# allowNumbers = FALSE
+# lowercase = TRUE
+# continue = NULL
 
 
 
