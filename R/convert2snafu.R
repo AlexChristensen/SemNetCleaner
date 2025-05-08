2244b1ff23a018296548472f030b4ffd592ba342
Alexander Christensen <alexpaulchristensen@gmail.com>
2025-05-08 09:33:15 -0500

    updates to remain on CRAN

--- R/convert2snafu.R
+++ R/convert2snafu.R
@@ -1,53 +1,56 @@
 #' Converts \code{\link[SemNetCleaner]{textcleaner}} object
 #' to a SNAFU GUI format
-#' 
+#'
 #' @description Converts \code{\link[SemNetCleaner]{textcleaner}} object
 #' to a SNAFU GUI format (only works for fluency data)
-#' 
+#'
 #' @param ... Matrix or data frame.
 #' A clean response matrices
-#' 
+#'
 #' @param category Character.
 #' Category of verbal fluency data
-#' 
+#'
 #' @return A .csv file formatted for SNAFU
-#' 
+#'
 #' @details The format of the file has 7 columns:
 #' \itemize{
-#' \item{id}{Defaults to the row names of the inputted \code{data}}
-#' 
-#' \item{listnum}{The list number for the fluency category. Defaults to 0.
-#' Future implementations will allow more lists}
-#' 
-#' \item{category}{The verbal fluency category that is input into the
-#' \code{category} argument}
-#' 
-#' \item{item}{The verbal fluency responses for every participant}
-#' 
-#' \item{RT}{Response time. Currently not implemented. Defaults to 0}
-#' 
-#' \item{RTstart}{Start of response time. Currently not implemented. Defaults to 0}
-#' 
-#' \item{group}{Names of groups. Defaults to the names of the objects input into
-#' the function (\code{...})}
+#'
+#' \item id --- Defaults to the row names of the inputted \code{data}
+#'
+#' \item listnum --- The list number for the fluency category. Defaults to 0.
+#' Future implementations will allow more lists
+#'
+#' \item category --- The verbal fluency category that is input into the
+#' \code{category} argument
+#'
+#' \item item --- The verbal fluency responses for every participant
+#'
+#' \item RT --- Response time. Currently not implemented. Defaults to 0
+#'
+#' \item RTstart --- Start of response time. Currently not implemented.
+#' Defaults to 0
+#'
+#' \item group --- Names of groups. Defaults to the names of the objects
+#' input into the function (\code{...})
+#'
 #' }
-#' 
+#'
 #' @examples
 #' # Convert data to SNAFU
 #' if(interactive())
 #' {convert2snafu(open.clean, category = "animals")}
-#' 
-#' @references 
+#'
+#' @references
 #' # For SNAFU, see:
 #' Zemla, J. C., Cao, K., Mueller, K. D., & Austerweil, J. L. (2020).
 #' SNAFU: The Semantic Network and Fluency Utility.
 #' \emph{Behavior Research Methods}, 1-19.
 #' https://doi.org/10.3758/s13428-019-01343-w
-#' 
+#'
 #' @author Alexander Christensen <alexpaulchristensen@gmail.com>
-#' 
+#'
 #' @importFrom utils write.table
-#' 
+#'
 #' @export
 # Convert data to SNAFU
 # Updated 19.06.2022
@@ -55,88 +58,88 @@ convert2snafu <- function (..., category)
 {
   # Data list
   data.list <- list(...)
-  
+
   # Initialize snafu matrix
   snafu.mat <- matrix(0, nrow = 0, ncol = 7)
   colnames(snafu.mat) <- c("id", "listnum", "category", "item", "RT", "RTstart", "group")
-  
+
   if(length(data.list) == 1)
   {
-    
+
   }else{
-    
+
     # Get group names
     name <- as.character(substitute(list(...)))
     name <- name[-which(name=="list")]
-    
+
     for(i in 1:length(data.list))
     {
       # Target data
       target.data <- as.matrix(data.list[[i]])
-      
+
       # Number of possible responses
       n <- ncol(target.data)
-      
+
       # IDs
       if(is.null(row.names(target.data)))
       {id <- paste("A", 1:nrow(target.data), sep = "")
       }else{id <- paste("A", formatC(as.numeric(row.names(target.data)), digits = 2, format = "d", flag = 0), sep = "")}
-      
+
       for(j in 1:nrow(target.data))
       {
         # Target participant
         target.part <- target.data[j,]
-        
+
         # Item
         item <- na.omit(target.part)
-        
+
         # Target ID
         target.id <- rep(id[j], length(item))
-        
+
         # List number
         listnum <- rep(0, length(item))
-        
+
         # Category
         categorey <- rep(category, length(item))
-        
+
         # RT
-        RT <- rep(0, length(item)) 
-        
+        RT <- rep(0, length(item))
+
         # RTstart
         RTstart <- rep(0, length(item))
-        
+
         # Group
         group <- rep(name[i], length(item))
-        
+
         # Bind data
         target.mat <- cbind(target.id, listnum,
                             categorey, item,
                             RT, RTstart, group)
-        
+
         row.names(target.mat) <- NULL
         colnames(target.mat) <- colnames(snafu.mat)
-        
+
         # Append snafu matrix
         snafu.mat <- rbind(snafu.mat, target.mat)
       }
     }
-    
+
   }
-  
+
   # Choose directory
   DIR <- easycsv::choose_dir()
-  
+
   # Get file name
   FILENAME <- readline("Name of file: ")
-  
+
   # Set up path
   PATH <- paste(DIR, FILENAME, sep = "/")
   PATH <- gsub("\\\\", "/", PATH)
   PATH <- paste(PATH, "csv", sep = ".")
-  
+
   write.table(snafu.mat, file = PATH,
               quote = FALSE, sep = ",", row.names = FALSE)
-  
+
   # Message to user
   message(paste("SNAFU formatted file was saved in: "), PATH)
 }
--- R/textcleaner.R
+++ R/textcleaner.R
@@ -1,28 +1,28 @@
 #' Text Cleaner
-#' 
+#'
 #' @description An automated cleaning function for spell-checking, de-pluralizing,
 #' removing duplicates, and binarizing text data
-#' 
+#'
 #' @param data Matrix or data frame.
-#' 
+#'
 #' For \code{task = "fluency"}, data are expected to
 #' follow wide formatting (IDs are the row names
 #' and are \strong{not} a column in the matrix
 #' or data frame):
-#' 
+#'
 #' \tabular{cccc}{
-#' 
+#'
 #' \code{row.names} \tab Response 1 \tab Response 2 \tab Response n \cr
 #' ID_1 \tab 1 \tab 2 \tab n \cr
 #' ID_2 \tab 1 \tab 2 \tab n \cr
-#' ID_n \tab 1 \tab 2 \tab n 
+#' ID_n \tab 1 \tab 2 \tab n
 #' }
-#' 
+#'
 #' For \code{task = "free"}, data are expected to
 #' follow long formatting:
-#' 
+#'
 #' \tabular{ccc}{
-#' 
+#'
 #' ID \tab Cue \tab Response \cr
 #' 1 \tab 1 \tab 1 \cr
 #' 1 \tab 1 \tab 2 \cr
@@ -51,172 +51,170 @@
 #' n \tab n \tab 1 \cr
 #' n \tab n \tab 2 \cr
 #' n \tab n \tab n
-#' 
+#'
 #' }
-#' 
+#'
 #' @param type Character vector.
 #' Type of task to be preprocessed.
 #' \itemize{
-#' 
-#' \item{\code{"fluency"}}
-#' {Verbal fluency data (e.g., categories, phonological, synonyms)}
-#' 
-#' \item{\code{"free"}}
-#' {Free association data (e.g., cue terms or words)}
-#' 
+#'
+#' \item \code{"fluency"} --- Verbal fluency data (e.g., categories, phonological,
+#' synonyms)
+#'
+#' \item \code{"free"} --- Free association data (e.g., cue terms or words)
+#'
 #' }
-#' 
+#'
 #' @param miss Numeric or character.
 #' Value for missing data.
 #' Defaults to \code{99}
-#' 
+#'
 #' @param partBY Character.
 #' Are participants by row or column?
 #' Set to \code{"row"} for by row.
 #' Set to \code{"col"} for by column
-#' 
+#'
 #' @param dictionary Character vector.
 #' Can be a vector of a corpus or any text for comparison.
 #' Dictionary to be used for more efficient text cleaning.
 #' Defaults to \code{NULL}, which will use \code{\link[SemNetDictionaries]{general.dictionary}}
-#' 
+#'
 #' Use \code{dictionaries()} or \code{find.dictionaries()} for more options
 #' (See \code{\link{SemNetDictionaries}} for more details)
-#' 
+#'
 #' @param spelling Character vector.
 #' English spelling to be used.
 #' \itemize{
-#' 
-#' \item{\code{"UK"}}
-#' {For British spelling (e.g., colour, grey, programme, theatre)}
-#' 
-#' \item{\code{"US"}}
-#' {For American spelling (e.g., color, gray, program, theater)}
-#' 
+#'
+#' \item \code{"UK"} --- For British spelling (e.g., colour, grey,
+#' programme, theatre)
+#'
+#' \item \code{"US"} --- For American spelling (e.g., color, gray, program,
+#' theater)
+#'
 #' }
-#' 
+#'
 #' @param add.path Character.
 #' Path to additional dictionaries to be found.
 #' DOES NOT search recursively (through all folders in path)
 #' to avoid time intensive search.
 #' Set to \code{"choose"} to open an interactive directory explorer
-#' 
+#'
 #' @param keepStrings Boolean.
 #' Should strings be retained or separated?
 #' Defaults to \code{FALSE}.
 #' Set to \code{TRUE} to retain strings as strings
-#' 
+#'
 #' @param allowPunctuations Character vector.
 #' Allows punctuation characters to be included in responses.
 #' Defaults to \code{"-"}.
 #' Set to \code{"all"} to keep all punctuation characters
-#' 
+#'
 #' @param allowNumbers Boolean.
 #' Defaults to \code{FALSE}.
 #' Set to \code{TRUE} to keep numbers in text
-#' 
+#'
 #' @param lowercase Boolean.
 #' Should words be converted to lowercase?
 #' Defaults to \code{TRUE}.
 #' Set to \code{FALSE} to keep words as they are
-#' 
+#'
 #' @param keepLength Numeric.
 #' Maximum number of words allowed in a response.
 #' Defaults to \code{NULL}.
 #' Set a number to keep responses with words less than
 #' or equal to the number (e.g., \code{3} will keep responses
 #' with three or less words)
-#' 
+#'
 #' @param keepCue Boolean.
 #' Should cue words be retained in the responses?
 #' Defaults to \code{FALSE}.
 #' Set to \code{TRUE} to allow cue words to be retained
-#' 
+#'
 #' @param continue List.
 #' A result previously unfinished that still needs to be completed.
 #' Allows you to continue to manually spell-check their data
 #' after you've closed or errored out.
 #' Defaults to \code{NULL}
-#' 
+#'
 #' @return This function returns a list containing the following objects:
-#' 
+#'
 #' \item{binary}{A matrix of responses where each row represents a participant
 #' and each column represents a unique response. A response that a participant has provided is a '\code{1}'
 #' and a response that a participant has not provided is a '\code{0}'}
 #'
 #' \item{responses}{A list containing two objects:
-#' 
+#'
 #' \itemize{
-#' 
-#' \item{\code{clean}}
-#' {A response matrix that has been spell-checked and de-pluralized with duplicates removed.
-#' This can be used as a final dataset for analyses (e.g., fluency of responses)}
-#' 
-#' \item{\code{original}}
-#' {The original response matrix that has had white spaces before and
-#' after words response. Also converts all upper-case letters to lower case}
-#' 
+#'
+#' \item \code{clean} --- A response matrix that has been spell-checked
+#' and de-pluralized with duplicates removed.
+#' This can be used as a final dataset for analyses (e.g., fluency of responses)
+#'
+#' \item \code{original} --- The original response matrix that has had
+#' white spaces before and after words response. Also converts all
+#' upper-case letters to lower case
+#'
 #' }
-#' 
+#'
 #' }
 #'
 #' \item{spellcheck}{A list containing three objects:
-#' 
+#'
 #' \itemize{
-#' 
-#' \item{\code{full}}
-#' {All responses regardless of spell-checking changes}
-#' 
-#' \item{\code{auto}}
-#' {Only the incorrect responses that were changed during spell-check}
-#' 
+#'
+#' \item \code{full} --- All responses regardless of spell-checking changes
+#'
+#' \item \code{auto} --- Only the incorrect responses that were changed
+#' during spell-check
+#'
 #' }
-#' 
+#'
 #' }
-#' 
-#' \item{removed}{A list containing two objects: 
-#' 
+#'
+#' \item{removed}{A list containing two objects:
+#'
 #' \itemize{
-#' 
-#' \item{\code{rows}}
-#' {Identifies removed participants by their row (or column) location in the original data file}
-#' 
-#' \item{\code{ids}}
-#' {Identifies removed participants by their ID (see argument \code{data})}
-#' 
+#'
+#' \item \code{rows} --- Identifies removed participants by their row
+#' (or column) location in the original data file
+#'
+#' \item \code{ids} --- Identifies removed participants by their ID
+#' (see argument \code{data})
+#'
 #' }
-#' 
+#'
 #' }
-#' 
+#'
 #' \item{partChanges}{A list where each participant is a list index with each
 #' response that was been changed. Participants are identified by their ID (see argument \code{data}).
 #' This can be used to replicate the cleaning process and to keep track of changes more generally.
 #' Participants with \code{NA} did not have any changes from their original data
 #' and participants with missing data are removed (see \code{removed$ids})}
-#' 
+#'
 #' @examples
 #' # Toy example
 #' raw <- open.animals[c(1:10),-c(1:3)]
-#' 
+#'
 #' if(interactive())
 #' {
 #'     #Full test
 #'     clean <- textcleaner(open.animals[,-c(1,2)], partBY = "row", dictionary = "animals")
 #' }
-#' 
+#'
 #' @references
 #' Christensen, A. P., & Kenett, Y. N. (in press).
 #' Semantic network analysis (SemNA): A tutorial on preprocessing, estimating, and analyzing semantic networks.
 #' \emph{Psychological Methods}.
-#'   
+#'
 #' Hornik, K., & Murdoch, D. (2010).
 #' Watch Your Spelling!.
 #' \emph{The R Journal}, \emph{3}, 22-28.
-#' 
+#'
 #' @author Alexander Christensen <alexpaulchristensen@gmail.com>
-#' 
+#'
 #' @importFrom stats na.omit
-#' 
+#'
 #' @export
 # Text Cleaner----
 # Updated 21.06.2022
@@ -238,47 +236,47 @@ textcleaner <- function(
   if(!is.null(continue)){
     type <- continue$type
   }
-  
+
   # Check for type
   if(missing(type)){
-    
+
     type <- ifelse(ncol(data) > 3, "fluency", "free")
-    
+
     message(
       paste(
         'Assuming semantic task type based on data structure: "',
         type, '"', sep = ""
       )
     )
-    
+
   }else{
     type <- match.arg(type)
   }
-  
+
   # Keep strings
   if(type == "free"){
-    
+
     if(missing(keepStrings)){
       keepStrings <- TRUE
       message("'keepStrings' is set to TRUE for free association tasks by default. Set argument to change this behavior.")
     }
-    
+
     if(missing(dictionary)){
       dictionary <- "cocaspell"
     }
-    
+
   }
-  
+
   # Warning for keepStrings
   # if(isTRUE(keepStrings)){
   #   message("Keeping strings intact is a new feature. There may be bugs or unexpected behavior.")
   #   message("\nPlease send issues to:")
   #   message("\nhttps://github.com/AlexChristensen/SemNetCleaner/issues")
   # }
-  
+
   # Check for missing arguments
   if(is.null(continue)){
-    
+
     ## Spelling
     if(missing(spelling)){
       spelling <- "US"
@@ -287,17 +285,17 @@ textcleaner <- function(
     }else{
       spelling <- match.arg(spelling)
     }
-    
+
     ## Allow punctuations
     if(missing(allowPunctuations)){
       allowPunctuations <- "-"
     }
-    
+
   }
-  
+
   # Check for type
   if(type == "fluency"){
-    
+
     ## Divert to fluency textcleaner
     res <- textcleaner.fluency(
       data = data, miss = miss, partBY = partBY,
@@ -308,10 +306,10 @@ textcleaner <- function(
       keepLength = keepLength,
       continue = continue
     )
-    
-    
+
+
   }else if(type == "free"){
-    
+
     ## Divert to free textcleaner
     res <- textcleaner.free(
       data = data, miss = miss,
@@ -322,9 +320,9 @@ textcleaner <- function(
       keepLength = keepLength, keepCue = keepCue,
       continue = continue
     )
-    
+
   }
-  
+
   return(res)
 }
 #----
